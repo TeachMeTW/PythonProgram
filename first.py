9066cfb3c4a3f6a254aa7d979560e160cd403e25
Robin <teachmetheways652@gmail.com>
2022-05-12 13:13:02 -0700

    Added practice

--- first.py
+++ first.py
@@ -1,5 +1,14 @@
 print('Hello, world')
 
+print(3+4)
+print("Hello")
+print("World")
+
+print("My favorite number are", 3+4, "and", 3+ 10) # error without the comma after and
+print("Goodbye")
+print()
+print("Hope to see you again")
+
 class TestClass(object):
     def __init__(self, name, age):
         self.name = name
@@ -11,6 +20,10 @@ print("name = {}, age = {}".format(t.name, t.age))
 def functest(fname, age):
     print('Function test')
     p = TestClass(fname, age)
-    print(p.name, p.age)
+    print('New person name is ' + p.name + ' and age is ' +p.age + ' years old')
     
-functest('Robin','Simpson')
+print('Enter name for person')
+name = input()
+print('Enter age for person')
+age = input()
+functest(name, age)
