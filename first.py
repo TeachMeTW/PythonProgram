070910b29e821f63378b46c475323b3f2bba0d07
Robin <teachmetheways652@gmail.com>
2022-05-12 13:07:11 -0700

    Merged

--- first.py
+++ first.py
@@ -1,1 +1,16 @@
 print('Hello, world')
+
+class TestClass(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+t = TestClass("Joe Mama", 69)
+print("name = {}, age = {}".format(t.name, t.age))
+
+def functest(fname, age):
+    print('Function test')
+    p = TestClass(fname, age)
+    print(p.name, p.age)
+    
+functest('Robin','Simpson')
--- objtest.py
+++
@@ -1,8 +0,0 @@
-class TestClass(object):
-    def __init__(self, make, price):
-        self.make = make
-        self.price = price
-        self.on = False
-        
-t = TestClass("Joe", 8.99)
-print(t.price)
