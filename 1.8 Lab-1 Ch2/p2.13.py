9b80a9f53f7cfcb0281a06190d078b831db592b6
Robin <teachmetheways652@gmail.com>
2022-05-16 13:21:50 -0700

    Update

--- 1.8 Lab-1 Ch2/p2.13.py
+++ 1.8 Lab-1 Ch2/p2.13.py
@@ -1,20 +1,20 @@
 
 def main():
     print('\nWelcome to P2.13 string to int')
-    # What are the inputs?
-    
-    # Get user input and cast into a string
-    val = (input('\nEnter a number between 10,000 and 99,999 with comma: '))
-    
-    # After getting rid of the comma, check if number falls within the range
-    # Make sure the comma is in the 2 index
-    while (not((10000 < int(val.replace(',','')) < 99999) and (',' in val[2]) and (len(val)) == 6)) :
-            #if it is valid then break out of loop and print the value
-            # get rid of ,
-        print('Invalid Input')
-        val = (input('\nEnter a number between 10,000 and 99,999 with comma: '))
-        # if not valid print invalid
-    
-    print(int(val.replace(',','')))
-    
+    while(True):
+        try:
+            # What are the inputs?
+            # Get user input and cast into a string
+            val = (input('\nEnter a number between 10,000 and 99,999 with comma: '))
+            # After getting rid of the comma, check if number falls within the range
+            # Make sure the comma is in the 2 index
+            if ((10000 < int(val.replace(',','')) < 99999) and (',' in val[2]) and (len(val)) == 6):
+               
+                print(int(val.replace(',','')))
+                return
+            else:
+            # if not 5 digit, error 
+                print('Not valid')
+        except ValueError:
+            print('Invalid Input!')
 main()
