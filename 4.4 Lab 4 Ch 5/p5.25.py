3761963ff1fd5dc54e108515c4c7f973c026f952
Robin <teachmetheways652@gmail.com>
2022-06-02 11:50:26 -0700

    Added comments

--- 4.4 Lab 4 Ch 5/p5.25.py
+++ 4.4 Lab 4 Ch 5/p5.25.py
@@ -35,11 +35,14 @@ def printBarCode(zip):
         
     # Check digit calculation
     zipTotal = 0
+    
     # Add up all digits and sum is multiple of 10
     for y in range(0, len(zip)):
         zipTotal+=int(zip[y])
     # finds remainder and missing num check
+    # print(str(zipTotal))
     modulo = 10 - (zipTotal%10)
+    # print('Check: ' + str(modulo))
     printDigit(abs(modulo))
     
     # end
@@ -61,3 +64,19 @@ def main():
             print('\nEnter a valid zip code')
     
 main()
+
+#############################################
+#   EX; Zip is 94510
+#
+#   1st: 9 -> |:|::
+#   2nd: 4 -> :|::|
+#   3rd: 5 -> :|:|:
+#   4th: 1 -> :::||
+#   5th: 0 -> ||:::
+#   
+#   Check = 10 - 19%10 -> 10 - 9 -> 1
+#   Check: -> :::||
+#
+#   Final Bar -> ||:|:::|::|:|:|::::||||::::::|||
+#
+#
--- 4.4 Lab 4 Ch 5/p5.27.py
+++ 4.4 Lab 4 Ch 5/p5.27.py
@@ -20,26 +20,37 @@ numDict = [
 ## Turns roman to int
 # @param nothing, user input is taken inside function
 # no return void funct prints out the value
-
-
 def romanToInt():
     # get user input, change to upper just in case
     userIn = str(input("\nEnter roman numerals: ")).upper()
     
+    # indexes
     stringIndex = 0
     tupleIndex = 0
+    
+    # resulting number
     result = 0
+    
     # Loop through user input characters
     while stringIndex < len(userIn):
+        
         # Loop through tuple and check if the tuple word starts at the current index
+        # startswith returns true if string starts with specified value in this case the character at a given index
         while tupleIndex < len(numDict) and not userIn.startswith(numDict[tupleIndex][0], stringIndex):
+            # up the index count if not at end of dict and it doesn't start at the given index
+            # print('Current char ' + str(numDict[tupleIndex][0]))
+            # print('\n')
+            # print('Current string ind ' + str(stringIndex))
+            # print('\nTuple Index: ' + str(tupleIndex))
             tupleIndex += 1
+            
         # if tuple is not at end
         if tupleIndex < len(numDict):
             # add current index value to final
             result += numDict[tupleIndex][1]
             # up the index value by checking the length of the roman character
             stringIndex += len(numDict[tupleIndex][0])
+            
         # if tuple is at end
         else:
             # I thought of doing recursive down here but it looped out
@@ -61,3 +72,28 @@ def main():
             print('Enter a valid roman numeral')
     
 main()
+
+
+
+#######################################
+#   Ex: MMXXII -> 2022 expected
+#   1st: Current character matching -> M | Found yes -> 1000
+#   2nd: Current character matching -> M | Found yes -> 1000
+#   3rd: Curent char match -> CM | index string 2 | tuple index 0 | found no
+#   4th: Current char match -> D | index string 2 | tuple index 1 | found no
+#   5th: Current char match -> CD | index string 2 | tuple index 2 | found no
+#   6th: Current char match -> C | index string 2 | tuple index 3 | found no
+#   7th: Current char match -> XC | index string 2| tuple index 4 | found no
+#   8th: Current char match -> L | index string 2 | tuple index 5 | found no
+#   9th: Current char match -> XL | index string 2 | tuple index 6 | found no
+#   10th: Current char match -> X | index string 2 | tuple index  7 | found yes -> 10
+#   11th: Current char match -> X | index string 3 | tuple index  7 | found yes -> 10
+#   12th: Current char match -> IX | index string 4 | tuple index  8 | found no
+#   13th: Current char match -> V | index string 4 | tuple index  9 | found no
+#   14th: Current char match -> IV | index string 4 | tuple index  10 | found no
+#   15th: Current char match -> I | index string 4 | tuple index  11 | found yes -> 1
+#   16th: Current char match -> I | index string 5 | tuple index  11 | found yes -> 1
+#
+#                           DONE
+#   Roman: MMXXII
+#   Decimal: 2022
